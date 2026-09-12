c1c1bf8641efbf5997143f9117836691c08e9487
dygeraldino <dygeraldino@uninorte.edu.co>
2026-09-12 00:46:28 -0500

    fix: execution with pythonw

--- config/logging_config.py
+++ config/logging_config.py
@@ -10,12 +10,13 @@ def setup_logging() -> None:
 
     logger.remove()
 
-    logger.add(
-        sys.stdout,
-        level=settings.log_level,
-        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-        colorize=True,
-    )
+    if sys.stdout is not None:
+        logger.add(
+            sys.stdout,
+            level=settings.log_level,
+            format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+            colorize=True,
+        )
 
     logger.add(
         log_dir / "daemon_{time:YYYY-MM-DD}.log",
